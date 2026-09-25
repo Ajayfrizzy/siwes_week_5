d3454ce1bc66de4954a1f6f969fc072e6cabe4a7
Ajayfrizzy <ajayholuwaseun@gmail.com>
2026-09-25 11:30:22 +0100

    Updated the README file

---
+++ index.js
@@ -0,0 +1,40 @@
+/* Task given to Student 9 */
+
+/* Task 1 */
+
+let city = "Miami";
+let country = "United State of America";
+
+console.log("I live in" + " " + city + "," + " " + country)
+console.log(`I live in ${city}, ${country}`)
+
+/* Task 2 */
+
+let first_set_of_students = 30;
+let second_set_of_students = 10;
+
+let totalStudents = first_set_of_students + second_set_of_students;
+
+console.log(totalStudents);
+
+/* Task 3 */
+
+let students = 40;
+
+if (students >= 35){
+    console.log("The class is full")
+}else {
+    console.log("There is still space")
+}
+
+/* Task 4 */
+
+let passengers =15;
+
+console.log(++passengers)
+
+/* Task 5 */
+
+let seats = 15;
+
+console.log(--seats)
